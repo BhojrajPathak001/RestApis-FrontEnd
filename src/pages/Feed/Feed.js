9615d811933a086c4cbcc0ca004d0bdc2c0ed687
indeed <bhojrajpathak001@gmail.com>
2024-05-25 21:46:36 +0530

    done with websocket

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -39,7 +39,18 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    openSocket("http://localhost:8080");
+    const socket = openSocket("http://localhost:8080");
+    socket.on("posts", (data) => {
+      if (data.action === "create") {
+        this.addPost(data.post);
+      }
+      if (data.action === "update") {
+        this.updatePost(data.post);
+      }
+      if (data.action === "delete") {
+        this.loadPosts();
+      }
+    });
   }
 
   loadPosts = (direction) => {
@@ -85,10 +96,30 @@ class Feed extends Component {
     this.setState((prevState) => {
       const updatedPosts = [...prevState.posts];
       if (prevState.postPage === 1) {
-        updatedPosts.pop();
+        if (prevState.posts.length >= 2) {
+          updatedPosts.pop();
+        }
         updatedPosts.unshift(post);
       }
-      return { posts: updatedPosts, totalPosts: prevState.totalPosts + 1 };
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1,
+      };
+    });
+  };
+
+  updatePost = (post) => {
+    this.setState((prevState) => {
+      const updatedPosts = [...prevState.posts];
+      const updatedPostIndex = updatedPosts.findIndex(
+        (p) => p._id === post._id
+      );
+      if (updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post;
+      }
+      return {
+        posts: updatedPosts,
+      };
     });
   };
 
@@ -173,17 +204,7 @@ class Feed extends Component {
           createdAt: resData.post.createdAt,
         };
         this.setState((prevState) => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              (p) => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
-          }
           return {
-            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false,
